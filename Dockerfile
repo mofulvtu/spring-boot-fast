d71b28c306298414ae8bfed7ddea1786e56d562a
liuzg <1172727371@qq.com>
2020-05-13 23:06:30 +0800

    init

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,4 @@ VOLUME /tmp
 COPY target/spring-boot-fast-0.0.1-SNAPSHOT.jar app.jar
 RUN bash -c "touch /app.jar"
 EXPOSE 8080
-#ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
 ENTRYPOINT ["java","-jar","app.jar"]
--- src/main/java/com/liuzg/module/TestController.java
+++ src/main/java/com/liuzg/module/TestController.java
@@ -1,52 +1,44 @@
 package com.liuzg.module;
 
-import com.google.common.collect.ImmutableMap;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
-/***
- * @author <a href="mailto:xiaoymin@foxmail.com">xiaoymin@foxmail.com</a>
- * 2017/04/20 14:01
- */
-@Api(value = "测试",tags = "测试用例")
+/**
+ * <pre>
+ *  Desc: test
+ * </pre>
+ *
+ * @author liuzg
+ * @date 2020/5/13 21:54
+ **/
+@Api(value = "测试", tags = "测试用例")
 @RestController
-@RequestMapping("/api/test")
+@RequestMapping("/api")
 public class TestController {
 
 
-    @ApiOperation(value = "puttest",notes = "puttest测试说明2")
-    @PutMapping(value = "/puttest")
-    @ApiImplicitParam(value = "code",name = "code",dataType = "string",paramType = "query",defaultValue = "abc")
-    public RestMessage puttest(String code){
+    @ApiOperation(value = "getTest", notes = "pgettest测试说明1")
+    @GetMapping(value = "/getTest")
+    @ApiImplicitParam(value = "code", name = "code", dataType = "string", paramType = "query", defaultValue = "abc")
+    public RestMessage getTest(String code) {
         return new RestMessage(code);
     }
 
-    @ApiOperation(value = "deletetest",notes = "deletetest测试说明2")
-    @DeleteMapping(value = "/deletetest")
-    @ApiImplicitParam(value = "code",name = "code",dataType = "string",paramType = "query",example = "abcefg")
-    public RestMessage deletetest(@RequestParam(value = "code") String code){
+    @ApiOperation(value = "putTest", notes = "putTest测试说明2")
+    @PutMapping(value = "/putTest")
+    @ApiImplicitParam(value = "code", name = "code", dataType = "string", paramType = "query", defaultValue = "abc")
+    public RestMessage putTest(String code) {
         return new RestMessage(code);
     }
 
-
-
-    @PostMapping("/reqbody3")
-    @ApiOperation(value = "header参数",notes = "header参数")
-    @ApiImplicitParams({
-            @ApiImplicitParam(value = "code",name = "code",dataType = "string",paramType = "query"),
-            @ApiImplicitParam(value = "headerparam",name = "headerparam",dataType = "string",paramType = "header"),
-            @ApiImplicitParam(value = "page",name = "page",dataType = "int",paramType = "query"),
-            @ApiImplicitParam(value = "page1",name = "page1",dataType = "Long",paramType = "query")
-    })
-    public RestMessage reqbody3(@RequestHeader(value = "headerparam") String headerparam,@RequestParam(value = "code") String code,
-                                @RequestParam(value = "page") int page,@RequestParam(value = "page1") Long page1){
-        return new RestMessage(ImmutableMap.of("code",code,"header",headerparam,"page",page,"page1",page1));
+    @ApiOperation(value = "deleteTest", notes = "deleteTest测试说明3")
+    @DeleteMapping(value = "/deleteTest")
+    @ApiImplicitParam(value = "code", name = "code", dataType = "string", paramType = "query", example = "abcefg")
+    public RestMessage deleteTest(@RequestParam(value = "code") String code) {
+        return new RestMessage(code);
     }
-
-
 
 
 }
